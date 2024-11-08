a42afa15515033e6ba4f7eedd60945116f2b27b6
Andy Fillebrown <andy@docEdub.com>
2024-11-07 22:29:25 -0500

    Failed attempt to use playwright with dynamic import
    ```
    npm test
    
    > test
    > playwright test
    
    Running 1 test using 1 worker
    
      ✘  1 webAudioStaticSound.test.ts:3:1 › StaticSoundBuffer (206ms)
    
      1) webAudioStaticSound.test.ts:3:1 › StaticSoundBuffer ───────────────────────────────────────────
    
        Error: Cannot find module '/Users/andy/-/code/microsoft/240709-audio-engine-v2-testing-esm/node_modules/@babylonjs/core/Audio/v2/webAudio/webAudioEngine' imported from /Users/andy/-/code/microsoft/240709-audio-engine-v2-testing-esm/src/tests/webAudioStaticSound.ts
        Did you mean to import "file:///Users/andy/-/code/3rdparty/Babylon.js/packages/public/@babylonjs/core/Audio/v2/webAudio/webAudioEngine.js"?
    
      1 failed
        webAudioStaticSound.test.ts:3:1 › StaticSoundBuffer ────────────────────────────────────────────
    ```

--- tests/webAudioStaticSound.test.ts
+++ tests/webAudioStaticSound.test.ts
@@ -1,13 +1,6 @@
-import { CreateAudioEngineAsync } from "@babylonjs/core/Audio/v2/webAudio/webAudioEngine";
-import { CreateSoundBufferAsync } from "@babylonjs/core/Audio/v2/webAudio/webAudioStaticSound";
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("StaticSoundBuffer", async ({ page }) => {
-    console.log("Running WebAudioStaticSound tests...");
-
-    const engine = await CreateAudioEngineAsync();
-    const buffer = await CreateSoundBufferAsync(engine, { sourceUrl: "https://amf-ms.github.io/AudioAssets/testing/3-count.mp3" });
-
-    expect(engine.mainOutput).toBeDefined();
-    expect(buffer).toBeDefined();
+    const BABYLON = await import("../src/tests/webAudioStaticSound");
+    await BABYLON.test();
 });
